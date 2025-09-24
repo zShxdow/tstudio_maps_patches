1bb84d153a6abe5cfc3bb718a359ad8068ca4a63
zShxdow <shxdowyt@gmail.com>
2025-09-24 13:26:21 +0200

    add patch for mrpd

---
+++ tstudio_zpatch_mrpark_th_ls_mrpd/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+lua54 'yes'
+game 'gta5'
+
+author 'tstudio'
+description 'Mission Row Park, Legion Square & Tropical Heights & Mission Row PD'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+  'stream/*.*',
+}
